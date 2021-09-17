808680b13114f200a8096ce91173153633b908d6
Fernando Pimenta <fosilva@brasilseg.com.br>
2021-09-17 16:22:07 -0300

    Dot net rest api

---
+++ pimentadotnetapirest/Controllers/FernandoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace pimentadotnetapirest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FernandoController : ControllerBase
+    {
+       
+        [HttpGet]
+        public List<WeatherForecast> ComLista()
+        {
+            List<WeatherForecast> list = new List<WeatherForecast>();
+            WeatherForecast a = new WeatherForecast { Fernando = "fefe" };
+
+            list.Add(a);
+
+            return list;
+        }
+
+
+        [HttpPost]
+        public List<WeatherForecast> Post(WeatherForecast b)
+        {
+            List<WeatherForecast> list = new List<WeatherForecast>();
+            list.Add(b);
+            return list;
+        }
+
+    }
+}
---
+++ pimentadotnetapirest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pimentadotnetapirest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var a =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Fernando = "programa .net"
+            })
+            .ToArray();
+
+            return a;
+        }
+
+        [Route("lista")]
+        [HttpGet]
+        public List<WeatherForecast> ComLista()
+        {
+            List<WeatherForecast> list = new List<WeatherForecast>();
+            WeatherForecast a = new WeatherForecast { Fernando = "fefe" };
+
+            list.Add(a);
+
+            return list;
+        }
+    }
+}
